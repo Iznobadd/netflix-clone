0e2677c5e02477606d596fb4e74794962f1213b9
Brandon <brandon.cebi.pro@gmail.com>
2023-05-09 11:59:48 +0200

    finish FAQ hero page

--- client/src/components/hero/Faq.tsx
+++ client/src/components/hero/Faq.tsx
@@ -1,12 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Plus from "../svg/Plus";
+import { Question } from "../../interfaces";
+import FormEmail from "./FormEmail";
 
 const Faq = () => {
-  const [collapse, setCollapse] = useState(false);
+  const [questions, setQuestions] = useState<Question[]>([]);
+  useEffect(() => {
+    fetch("questions.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+      .then((error) => console.error(error));
+  }, []);
 
-  const toggleCollapse = () => {
-    setCollapse((prevCollapse) => !prevCollapse);
-  };
+  function showAnswer(index: number) {
+    return () => {
+      const updatedQuestions = [...questions];
+      updatedQuestions[index].showAnswer = !updatedQuestions[index].showAnswer;
+      setQuestions(updatedQuestions);
+    };
+  }
+
   return (
     <div className="min-h-[auto] relative flex justify-center text-white h-full box-border text-center py-[3.5rem] md:py-[4.5rem]">
       <div className="m-auto max-w-[calc(100%-3rem)] md:max-w-[calc(100%-4rem)] xl:max-w-[calc(83.33%-6rem)] 2xl:max-w-[calc(66.66%-6rem)] flex items-center flex-col text-center w-full">
@@ -16,200 +34,42 @@ const Faq = () => {
           </h2>
           <div className="mt-[1.5rem]">
             <ul className="text-[1.125rem] lg:text-[1.5rem]">
-              <li className="mb-[0.5rem]">
-                <h3 className="flex mb-[0.0.625rem] relative bg-[rgba(45,45,45,1)] text-white ">
-                  <button
-                    className="p-[1.5rem] appearance-none bg-none rounded-none w-full cursor-pointer flex justify-between items-center text-left"
-                    onClick={toggleCollapse}
-                  >
-                    <span>Netflix, qu'est-ce que c'est ?</span>
-                    <Plus />
-                  </button>
-                </h3>
-                <div
-                  className={
-                    "overflow-hidden collapse max-h-0 px-[1.5rem] text-left bg-[rgba(45,45,45,1)] text-white " +
-                    (collapse ? "max-h-[75rem] !visible py-[1.5rem] " : "")
-                  }
-                >
-                  <span
+              {questions.map((item, index) => (
+                <li className="mb-[0.5rem]">
+                  <h3 className="flex mb-[0.0.625rem] relative bg-[rgba(45,45,45,1)] text-white ">
+                    <button
+                      className="p-[1.5rem] appearance-none bg-none rounded-none w-full cursor-pointer flex justify-between items-center text-left"
+                      onClick={showAnswer(index)}
+                    >
+                      <span>{item.question}</span>
+                      <Plus />
+                    </button>
+                  </h3>
+                  <div
                     className={
-                      "collapse text-left text-white " +
-                      (collapse ? "!visible" : "")
+                      "overflow-hidden max-h-0 px-[1.5rem] text-left bg-[rgba(45,45,45,1)] text-white " +
+                      (item.showAnswer
+                        ? "max-h-[75rem] visible py-[1.5rem] "
+                        : "collapse")
                     }
                   >
-                    Netflix est un service de streaming qui propose une vaste
-                    sélection de séries, films, animes, documentaires et autres
-                    programmes sur des milliers d'appareils connectés à
-                    Internet.
-                    <br />
-                    <br />
-                    Regardez tout ce que vous voulez, quand vous voulez, à un
-                    prix mensuel très attractif. Découvrez de nouveaux films et
-                    séries chaque semaine, il y en a pour tous les goûts !
-                  </span>
-                </div>
-              </li>
-              <li className="mb-[0.5rem]">
-                <h3 className="flex mb-[0.0.625rem] relative bg-[rgba(45,45,45,1)] text-white ">
-                  <button
-                    className="p-[1.5rem] appearance-none bg-none rounded-none w-full cursor-pointer flex justify-between items-center text-left"
-                    onClick={toggleCollapse}
-                  >
-                    <span>Combien coûte Netflix ?</span>
-                    <Plus />
-                  </button>
-                </h3>
-                <div
-                  className={
-                    "overflow-hidden collapse max-h-0 px-[1.5rem] text-left bg-[rgba(45,45,45,1)] text-white " +
-                    (collapse ? "max-h-[75rem] !visible py-[1.5rem] " : "")
-                  }
-                >
-                  <span
-                    className={
-                      "collapse text-left text-white " +
-                      (collapse ? "!visible" : "")
-                    }
-                  >
-                    Regardez Netflix sur votre smartphone, tablette, Smart TV,
-                    ordinateur ou appareil de streaming, le tout pour un tarif
-                    mensuel fixe. Les offres vont de 5,99 € à 17,99 € par mois.
-                    Pas de contrat ni de frais supplémentaires.
-                  </span>
-                </div>
-              </li>
-              <li className="mb-[0.5rem]">
-                <h3 className="flex mb-[0.0.625rem] relative bg-[rgba(45,45,45,1)] text-white ">
-                  <button
-                    className="p-[1.5rem] appearance-none bg-none rounded-none w-full cursor-pointer flex justify-between items-center text-left"
-                    onClick={toggleCollapse}
-                  >
-                    <span>Où puis-je regarder Netflix ?</span>
-                    <Plus />
-                  </button>
-                </h3>
-                <div
-                  className={
-                    "overflow-hidden collapse max-h-0 px-[1.5rem] text-left bg-[rgba(45,45,45,1)] text-white " +
-                    (collapse ? "max-h-[75rem] !visible py-[1.5rem] " : "")
-                  }
-                >
-                  <span
-                    className={
-                      "collapse text-left text-white " +
-                      (collapse ? "!visible" : "")
-                    }
-                  >
-                    Netflix, c'est où vous voulez, quand vous voulez.
-                    Connectez-vous à votre compte pour regarder Netflix en ligne
-                    sur netflix.com depuis votre ordinateur ou tout appareil
-                    connecté à Internet avec l'application Netflix, comme les
-                    Smart TV, smartphones, tablettes, lecteurs de streaming et
-                    consoles de jeu.
-                    <br />
-                    <br />
-                    Vous pouvez aussi télécharger vos séries préférées avec
-                    l'application iOS, Android ou Windows 10. Téléchargez des
-                    titres pour les regarder sur votre appareil mobile, même
-                    sans connexion Internet. Emportez Netflix partout avec vous.
-                  </span>
-                </div>
-              </li>
-              <li className="mb-[0.5rem]">
-                <h3 className="flex mb-[0.0.625rem] relative bg-[rgba(45,45,45,1)] text-white ">
-                  <button
-                    className="p-[1.5rem] appearance-none bg-none rounded-none w-full cursor-pointer flex justify-between items-center text-left"
-                    onClick={toggleCollapse}
-                  >
-                    <span>Comment puis-je annuler mon offre ?</span>
-                    <Plus />
-                  </button>
-                </h3>
-                <div
-                  className={
-                    "overflow-hidden collapse max-h-0 px-[1.5rem] text-left bg-[rgba(45,45,45,1)] text-white " +
-                    (collapse ? "max-h-[75rem] !visible py-[1.5rem] " : "")
-                  }
-                >
-                  <span
-                    className={
-                      "collapse text-left text-white " +
-                      (collapse ? "!visible" : "")
-                    }
-                  >
-                    Netflix offre une grande souplesse. Pas de contrat
-                    compliqué. Sans engagement. Deux clics suffisent pour
-                    annuler votre compte en ligne. Pas de frais d'annulation :
-                    ouvrez ou fermez votre compte à tout moment.
-                  </span>
-                </div>
-              </li>
-              <li className="mb-[0.5rem]">
-                <h3 className="flex mb-[0.0.625rem] relative bg-[rgba(45,45,45,1)] text-white ">
-                  <button
-                    className="p-[1.5rem] appearance-none bg-none rounded-none w-full cursor-pointer flex justify-between items-center text-left"
-                    onClick={toggleCollapse}
-                  >
-                    <span>Que puis-je regarder sur Netflix ?</span>
-                    <Plus />
-                  </button>
-                </h3>
-                <div
-                  className={
-                    "overflow-hidden collapse max-h-0 px-[1.5rem] text-left bg-[rgba(45,45,45,1)] text-white " +
-                    (collapse ? "max-h-[75rem] !visible py-[1.5rem] " : "")
-                  }
-                >
-                  <span
-                    className={
-                      "collapse text-left text-white " +
-                      (collapse ? "!visible" : "")
-                    }
-                  >
-                    Netflix propose un vaste catalogue comprenant notamment des
-                    longs-métrages, des documentaires, des séries, des animes et
-                    des programmes originaux Netflix primés. Regardez Netflix à
-                    volonté, quand vous le voulez.
-                  </span>
-                </div>
-              </li>
-              <li className="mb-[0.5rem]">
-                <h3 className="flex mb-[0.0.625rem] relative bg-[rgba(45,45,45,1)] text-white ">
-                  <button
-                    className="p-[1.5rem] appearance-none bg-none rounded-none w-full cursor-pointer flex justify-between items-center text-left"
-                    onClick={toggleCollapse}
-                  >
-                    <span>Est-ce que Netflix est adapté aux enfants ?</span>
-                    <Plus />
-                  </button>
-                </h3>
-                <div
-                  className={
-                    "overflow-hidden collapse max-h-0 px-[1.5rem] text-left bg-[rgba(45,45,45,1)] text-white " +
-                    (collapse ? "max-h-[75rem] !visible py-[1.5rem] " : "")
-                  }
-                >
-                  <span
-                    className={
-                      "collapse text-left text-white " +
-                      (collapse ? "!visible" : "")
-                    }
-                  >
-                    Netflix Jeunesse est inclus dans votre abonnement et offre
-                    un meilleur contrôle aux parents, ainsi qu'un espace dédié
-                    aux enfants, avec des films et des séries destinés à toute
-                    la famille.
-                    <br />
-                    <br />
-                    Les profils Jeunesse comportent des fonctionnalités de
-                    contrôle parental avec code PIN permettant de modifier la
-                    catégorie d'âge des contenus que vos enfants peuvent
-                    regarder et de bloquer des titres spécifiques.
-                  </span>
-                </div>
-              </li>
+                    <span
+                      className={
+                        "text-left text-white " +
+                        (item.showAnswer ? "visible" : "collapse")
+                      }
+                    >
+                      {item.answer}
+                    </span>
+                  </div>
+                </li>
+              ))}
             </ul>
+            <div className="mt-[2.85rem]">
+              <div className="text-center box-border max-w-[61.5rem] mx-auto px-[1.5rem] md:px-[2rem]">
+                <FormEmail />
+              </div>
+            </div>
           </div>
         </div>
       </div>
