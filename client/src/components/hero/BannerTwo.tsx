427e90a6cbed7f60766570b1de9970329cd85998
Brandon <brandon.cebi.pro@gmail.com>
2023-05-12 13:40:25 +0200

    fix svg and install apollo client dependencies

--- client/src/components/hero/BannerTwo.tsx
+++ client/src/components/hero/BannerTwo.tsx
@@ -25,8 +25,8 @@ const BannerTwo = () => {
               data-name="ChevronRight"
             >
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M7.29297 19.2928L14.5859 12L7.29297 4.70706L8.70718 3.29285L16.7072 11.2928C16.8947 11.4804 17.0001 11.7347 17.0001 12C17.0001 12.2652 16.8947 12.5195 16.7072 12.7071L8.70718 20.7071L7.29297 19.2928Z"
                 fill="currentColor"
               ></path>
